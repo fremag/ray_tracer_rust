ee1c1652cd48dee3cad049582529d6ffa05528f6
fremag <fremag.gh@gmail.com>
2023-11-03 15:59:00 +0100

    Colors: fix unit test

--- src/math.rs
+++ src/math.rs
@@ -2,6 +2,6 @@ pub(crate) type Float = f32;
 const EPSILON: Float = 0.0001;
 
 pub fn equals(a : Float, b : Float) -> bool {
-    let delta= a - b;
-    delta.abs() < EPSILON
+    let delta_abs = (a - b).abs();
+    delta_abs < EPSILON
 }
--- src/tests/color_tests.rs
+++ src/tests/color_tests.rs
@@ -37,6 +37,6 @@ fn hadamard_product_colors_test() {
     let c1 = Color{r: 1.0, g : 0.2, b: 0.4};
     let c2 = Color{r: 0.9, g : 1.0, b: 0.1};
     let c = c1 * c2;
-    let color = Color { r: 0.9, g: 0.2, b: 0.4 };
+    let color = Color { r: 0.9, g: 0.2, b: 0.04 };
     assert_eq!(c == color, true)
 }
