8cb73b1e678278a36b2ccea906cba8c87fad38fe
fremag <fremag.gh@gmail.com>
2023-11-04 16:48:07 +0100

    Canvas: save to ppm file

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,4 +1,7 @@
+use std::fs::File;
+use std::io::{Write, Error};
 use crate::colors::Color;
+use crate::math::Float;
 
 #[derive(Debug)]
 pub struct Canvas {
@@ -14,10 +17,68 @@ impl Canvas {
     }
 
     pub fn write_pixel(&mut self, x : usize, y : usize, color : Color) {
+        if x >= self.width {
+            return;
+        }
+        if y >= self.height {
+            return;
+        }
+
         self.pixels[y][x] = color;
     }
 
     pub fn pixel_at(&self, x : usize, y : usize) -> Color {
         self.pixels[y][x]
     }
+
+    pub fn ppm_header(&self) -> String {
+        format!("P3\n{} {}\n255\n", self.width, self.height)
+    }
+
+    pub fn pixel_data(&self) -> String {
+        let mut data = String::new();
+        let mut line = String::new();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let c = self.pixels[y][x];
+                self.push(&mut line, &mut data, c.r);
+                self.push(&mut line, &mut data, c.g);
+                self.push(&mut line, &mut data, c.b);
+            }
+            data.push_str(&line);
+            data.push('\n');
+            line.clear()
+        }
+
+        data
+    }
+
+    fn push(&self, line: &mut String, data: &mut String, value : Float) {
+        if line.len() >= 65 {
+            data.push_str(line);
+            data.push('\n');
+            line.clear()
+        }
+
+        let mut c = 255.0 * value;
+        if c < 0.0 {
+            c = 0.0;
+        }
+        if c > 255.0 {
+            c = 255.0;
+        }
+
+        let val_str = format!("{:.0}", c);
+        line.push_str(&val_str);
+        line.push(' ' );
+    }
+
+    pub(crate) fn save(&self, path: &str) -> Result<(), Error> {
+        let mut output = File::create(path)?;
+        write!(output, "{}", self.ppm_header())?;
+        write!(output, "{}", self.pixel_data())?;
+
+        Ok(())
+    }
 }
+
