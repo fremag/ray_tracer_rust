669cd22efafab03989e080306ca922c815f1076d
fremag <fremag.gh@gmail.com>
2024-02-01 21:57:25 +0100

    Refactoring: move files into lib

---
+++ ray-tracer-lib/src/lib.rs
@@ -0,0 +1,14 @@
+pub mod core;
+pub mod colors;
+pub mod object;
+pub mod material;
+pub mod lights;
+pub mod patterns;
+pub mod shapes;
+pub mod tests;
+pub mod world;
+pub mod canvas;
+pub mod camera;
+
+
+
