d841fe23fb801bf10a7d91ee4114d3db2755118f
fremag <fremag.gh@gmail.com>
2024-02-26 22:19:07 +0100

    Chap 16: CSG

--- ray-tracer-lib/src/tests/csg_tests.rs
+++ ray-tracer-lib/src/tests/csg_tests.rs
@@ -1,6 +1,9 @@
 #[cfg(test)]
 mod tests {
-    use crate::object::{build_cube, build_sphere};
+    use crate::core::ray::ray;
+    use crate::core::transform::translation;
+    use crate::core::tuple::{point, vector};
+    use crate::object::{build_cube, build_sphere, Object};
     use crate::shapes::csg::{Csg, CsgOperation};
 
     #[test]
@@ -47,5 +50,34 @@ mod tests {
         assert_eq!(Csg::intersection_allowed(&CsgOperation::Difference, false, true, false), true);
         assert_eq!(Csg::intersection_allowed(&CsgOperation::Difference, false, false, true), false);
         assert_eq!(Csg::intersection_allowed(&CsgOperation::Difference, false, false, false), false);
+    }
+
+    #[test]
+    fn a_ray_misses_a_csg_object_test() {
+        let csg = Object::new_csg(Csg::new(CsgOperation::Union, build_sphere(), build_cube()));
+
+        let r = ray(point(0.0, 2.0, -5.0), vector(0.0, 0.0, 1.0));
+
+        let xs = csg.intersect(&r);
+        assert_eq!(xs.count(), 0);
+    }
+
+    #[test]
+    fn a_ray_hits_a_csg_object_test() {
+        let s1 = build_sphere();
+        let mut s2 = build_sphere();
+        s2.set_transformation(translation(0.0, 0.0, 0.5));
+
+        let csg = Object::new_csg(Csg::new(CsgOperation::Union, s1.clone(), s2.clone()));
+
+        let r = ray(point(0.0, 0.0, -5.0), vector(0.0, 0.0, 1.0));
+        let xs = csg.intersect(&r);
+        assert_eq!(xs.count(), 2);
+
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[0].object, s1);
+
+        assert_eq!(xs[1].t, 6.5);
+        assert_eq!(xs[1].object, s2);
     }
 }
